546ab487afc481b84f81836f5ec75e830c06f9a9
Max Sun <maxlouiesun@gmail.com>
2020-04-02 21:00:43 -0700

    added range_map module

--- encoding.py
+++ encoding.py
@@ -4,27 +4,9 @@ import decimal
 from decimal import *
 from fractions import *
 from typing import NamedTuple, Dict
+from range_map import *
 
 decimal.getcontext().prec = 100
-
-
-class Range(NamedTuple):
-    '''Represents a range of Decimals'''
-    start: Decimal
-    end: Decimal
-
-    def length(self) -> Decimal:
-        '''Returns the length of the range'''
-        return self.end - self.start
-
-    def average(self) -> Decimal:
-        '''Returns the midpoint of the range'''
-        return (self.start + self.end) / 2
-
-    def __contains__(self, num) -> bool:
-        '''Returns whether the range contains <num>'''
-        return self.start <= num and self.end > num
-
 
 Model = Dict[str, Decimal]
 
@@ -32,44 +14,36 @@ TERMINATE_SYMBOL = '~'
 SYMBOLS = set(string.printable + TERMINATE_SYMBOL)
 
 
-def build_interval(model: Model, interval: Range) -> Dict[str, Range]:
-    interval_map = {}
+def build_interval(model: Model, interval: Range) -> RangeMap:
+    interval_map = RangeMap()
     accum = interval.start
     for char in sorted(model):
         prob = model[char]
         interval_size = prob * interval.length()
-        interval_map[char] = Range(accum, accum + interval_size)
+        interval_map.add_range(char, Range(accum, accum + interval_size))
         accum += interval_size
 
     return interval_map
 
 
 def encode(msg: str, model: Model) -> Decimal:
-    i = build_interval(model, Range(Decimal(0), Decimal(1)))
+    i = build_interval(model, Range(0, 1))
     char_interval = None
     for char in msg + TERMINATE_SYMBOL:
         char_interval = i[char]
         i = build_interval(model, char_interval)
-    if char_interval:
-        return char_interval.average()
-
-    raise Exception('No intervals found while encoding!')
+    return char_interval.average()
 
 
 def decode(encoded_msg: Decimal, model: Model) -> str:
-    i = build_interval(model, Range(Decimal(0), Decimal(1)))
+    i = build_interval(model, Range(0, 1))
     decoded_msg = ''
-    has_terminated = False
-    while not has_terminated:
-        for char in i:
-            if encoded_msg in i[char]:
-                i = build_interval(model, i[char])
-                if char == TERMINATE_SYMBOL:
-                    has_terminated = True
-                    break
-                else:
-                    decoded_msg += char
-
+    while True:
+        char = i.containing(encoded_msg)[0]
+        if char == TERMINATE_SYMBOL:
+            break
+        i = build_interval(model, i[char])
+        decoded_msg += char
     return decoded_msg
 
 
@@ -78,7 +52,6 @@ MSG = 'One small step for man...'
 pdf = {}
 for char in SYMBOLS:
     pdf[char] = Decimal(1 / (len(SYMBOLS)))
-
 
 c = encode(MSG, pdf)
 print(c)
---
+++ range_map.py
@@ -0,0 +1,76 @@
+
+import decimal
+from decimal import Decimal
+from typing import NamedTuple
+
+decimal.getcontext().prec = 100
+
+
+class Range:
+    '''Represents a range of Decimals'''
+    start: Decimal
+    end: Decimal
+
+    def __init__(self, start, end):
+        self.start = Decimal(start)
+        self.end = Decimal(end)
+
+    def __repr__(self) -> str:
+        return '(%0f, %0f)' % (self.start, self.end)
+
+    def length(self) -> Decimal:
+        '''Returns the length of the range'''
+        return self.end - self.start
+
+    def average(self) -> Decimal:
+        '''Returns the midpoint of the range'''
+        return (self.start + self.end) / 2
+
+    def __contains__(self, num) -> bool:
+        '''Returns whether the range contains <num>'''
+        return self.start <= num and self.end > num
+
+
+class RangeMap:
+    '''maps a Decimal to the Ranges which include it'''
+
+    ranges = {}
+    min = 0
+    max = 0
+
+    def add_range(self, range_id: str, rng: Range) -> None:
+        '''Add a IDed range to the RangeMap'''
+        if rng.start < self.min:
+            self.min = rng.start
+        if rng.end > self.max:
+            self.max = rng.end
+
+        self.ranges[range_id] = rng
+
+    def containing(self, value):
+        '''Find the ranges <value> belongs in'''
+        if value < self.min or value > self.max:
+            return []
+        results = []
+        for rng_id in self.ranges:
+            if value in self.ranges[rng_id]:
+                results.append(rng_id)
+        return results
+
+    def __setitem__(self, key, item):
+        self.ranges[key] = item
+
+    def __getitem__(self, key):
+        return self.ranges[key]
+
+
+class Node:
+
+    left: 'Node'
+    right: 'Node'
+    value: Range
+
+rm = RangeMap()
+rm.add_range('A', Range(0, 0.9))
+rm.add_range('B', Range(0.9, 1))
+
